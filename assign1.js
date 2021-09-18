50ab9c03b3d95a5fcd3a8cc4aee8e6dc3e393207
Wareewan Pongpunchaikul <68785361+wareewan-opal@users.noreply.github.com>
2021-09-19 03:20:11 +0700

    zodiac assignment

---
+++ assign1.js
@@ -0,0 +1,43 @@
+function zodiac(years) {
+    let result = years % 12;
+    switch (result) {
+        case 0:
+            console.log('monkey');
+            break;
+        case 1:
+            console.log('rooster');
+            break;
+        case 2:
+            console.log('dog');
+            break;
+        case 3:
+            console.log('pig');
+            break;
+        case 4:
+            console.log('rat');
+            break;
+        case 5:
+            console.log('ox');
+            break;
+        case 6:
+            console.log('tiger');
+            break;
+        case 7:
+            console.log('rabbit');
+            break;
+        case 8:
+            console.log('dragon');
+            break;
+        case 9:
+            console.log('snake');
+            break;
+        case 10:
+            console.log('horse');
+            break;
+        case 11:
+            console.log('sheep');
+            break;
+    }
+}
+zodiac(1900);
+console.log(zodiac);
